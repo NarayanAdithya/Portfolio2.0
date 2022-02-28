26086d8bf853545eae18cb19c8894a96248b149f
NarayanAdithya <narayanadithya1234@gmail.com>
2022-02-28 14:12:48 +0530

    Projects Done

--- app/models.py
+++ app/models.py
@@ -14,9 +14,10 @@ class TechStack(db.Model):
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     image_name = db.Column(db.String(30))
-    name = db.Column(db.String(10),unique=True, nullable=False)
-    description = db.Column(db.String(150), nullable=False)
+    name = db.Column(db.String(30),unique=True, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
     github = db.Column(db.String(120), nullable=False)
-    tag_urls = db.Column(db.String(120), nullable=True)
+    tag_urls = db.Column(db.String(500), nullable=True)
+    status = db.Column(db.Integer,default=1)
     def __repr__(self):
         return f'<Project {self.name}>'
